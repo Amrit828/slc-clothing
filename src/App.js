adbc05ecedcce5558f8d4cd8561f5109deb1726d
Amrit Gopinath <amritgopi06@gmail.com>
2020-08-16 12:42:55 +0530

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/shop/hats" component={HatsPage}/>
     </div>
   );
 }
